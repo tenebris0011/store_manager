00afb956ebd18b431181ef5b23696be0da6e2a99
Nathanial Wilson <wilson.nathanial95@gmail.com>
2022-09-22 13:14:48 -0500

    fix requirements and settings.

--- core/core/settings.py
+++ core/core/settings.py
@@ -29,12 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-<<<<<<< HEAD
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-=======
-ALLOWED_HOSTS = ['*']
->>>>>>> c6250c138447fc395607dc60fe3284ae8e3cba86
-
 
 # Application definition
 
